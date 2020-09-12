6a94475dc5480d6f90242cfacc0b3f2c42bec076
casternuts <show5960@gmail.com>
2020-09-12 16:20:26 +0900

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -71,11 +71,11 @@ function requestTick() {
 
 window.addEventListener('scroll', requestTick);
 
-let vh = window.innerHeight * 0.01;
-document.documentElement.style.setProperty("--vh", `${vh}px`);
+// let vh = window.innerHeight * 0.01;
+// document.documentElement.style.setProperty("--vh", `${vh}px`);
 
-window.addEventListener("resize", () => {
-    console.log("resize");
-    let vh = window.innerHeight * 0.01;
-    document.querySelector('body').style.setProperty("--vh", `${vh}px`);
-});
+// window.addEventListener("resize", () => {
+//     console.log("resize");
+//     let vh = window.innerHeight * 0.01;
+//     document.querySelector('body').style.setProperty("--vh", `${vh}px`);
+// });
