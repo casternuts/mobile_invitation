2876da40d5f629d77a3bf09d911ca348051c8aed
casternuts <show5960@gmail.com>
2020-09-12 15:26:06 +0900

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -77,5 +77,5 @@ document.documentElement.style.setProperty("--vh", `${vh}px`);
 window.addEventListener("resize", () => {
     console.log("resize");
     let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty("--vh", `${vh}px`);
+    document.querySelector('body').style.setProperty("--vh", `${vh}px`);
 });
